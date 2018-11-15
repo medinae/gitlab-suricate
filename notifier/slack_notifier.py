fc0ebb227a1c5ea925f69048bdcc03c90c777f10
Abdelkader Bouadjadja <abouadjadja@wynd.eu>
2018-11-15 09:28:11 +0400

    Fix file indentation

--- notifier/slack_notifier.py
+++ notifier/slack_notifier.py
@@ -6,11 +6,12 @@ class SlackNotifier:
 
 	def notify(self, message):
 		response = requests.post(
-		self.config['SLACK_WEBHOOK_URL'],
-		json = {
-			'text': message,
-			'channel': self.config['SLACK_CHANNEL_NAME']
-		})
+			self.config['SLACK_WEBHOOK_URL'],
+			json = {
+				'text': message,
+				'channel': self.config['SLACK_CHANNEL_NAME']
+			}
+		)
 
 		if (200 != response.status_code):
 			raise Exception('Error when trying to contact Slack API')
